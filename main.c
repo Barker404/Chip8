2f92f6a1eb85a525bffcb7d4ff02d73c7e3df882
Barker404 <lewisjbarker@btopenworld.com>
2014-01-12 16:36:48 +0000

    SDL Basics for I/O

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <SDL.h>
 #include "chip8.h"
-//input and output here
 
+SDL_Surface* screen;
+SDL_Event event;
+
+void setupGraphics()
+{
+	//Initialize SDL video
+	if (SDL_Init(SDL_INIT_VIDEO) <0)
+	{
+		printf("Error: SDL failed to initialize");
+		exit(1);
+	}
+	printf("Step 1 Success");
+
+	//Set screen caption
+	SDL_WM_SetCaption("Chip8 Emulator", "Chip8 Emulator");
+	printf("Step 2 Success");
+		
+	//Set video mode to 64 x 32
+	screen = SDL_SetVideoMode(64,32,0,0);
+	if (screen == NULL)
+	{
+		printf("Error:Couldn't set screen to 64 x 32");
+		exit(1);
+	}
+	printf("Step 3 Success");
+	SDL_Delay(1000);
+}
+
+void setupInput()
+{
+	
+}
+
+void drawGraphics()
+{
+	
+}
 
 int main(int argc, char **argv)
 {
@@ -28,20 +65,8 @@ int main(int argc, char **argv)
 		setKeys();
 	}
 
+	SDL_FreeSurface(screen);
+	SDL_Quit();
+
 	return 0;
-}
-
-void setupGraphics()
-{
-	
-}
-
-void setupInput()
-{
-	
-}
-
-void drawGraphics()
-{
-	
 }
