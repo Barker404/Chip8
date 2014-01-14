f29ee4f1b81c8428de0e8992533363c7679c2481
Barker404 <lewisjbarker@btopenworld.com>
2014-01-14 23:54:41 +0000

    SDL working

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <SDL.h>
+#include <SDL/SDL.h>
 #include "chip8.h"
 
 #define SCALE 4;
 
 SDL_Surface* screen;
 SDL_Event event;
-SDL_Rect *pixels[64][32];
+SDL_Rect pixels[64][32];
 
 SDL_PixelFormat *format;
+
+unsigned int testoo;
 
 unsigned int white;
 unsigned int black; 
@@ -17,25 +19,27 @@ unsigned int black;
 int main(int argc, char **argv)
 {
 	//Setup rendering and input
+	testoo = 0;
 	setupGraphics();
 	setupInput();
 
 	//Initialize Chip8 system and load game into memory
-	//initialize();
-	//loadGame("pong");
+	initialize();
+	loadGame("pong");
 
 	//Emulation loop
 	while(gameRunning)
 	{
 		//Emulate one cycle
-		//emulateCycle();
+		emulateCycle();
 
 		//If draw flag set, update screen
-		//if(drawFlag)
+		if(drawFlag)
 			drawGraphics();
 
 		//Store key press state
-		//setKeys();
+		setKeys();
+		testoo++;
 	}
 
 	SDL_FreeSurface(screen);
@@ -52,11 +56,9 @@ void setupGraphics()
 		printf("Error: SDL failed to initialize");
 		exit(1);
 	}
-	printf("Step 1 Success");
 
 	//Set screen caption
 	SDL_WM_SetCaption("Chip8 Emulator", "Chip8 Emulator");
-	printf("Step 2 Success");
 		
 	//Set video mode to 64 x 32
 	int h;
@@ -69,32 +71,21 @@ void setupGraphics()
 		printf("Error:Couldn't set screen to %d x %d", w, h);
 		exit(1);
 	}
-	printf("Step 3 Success");
 
 	for (int i = 0; i < 32; i++)		//looping across y
 	{
 		for (int j = 0; j < 64; j++)	//looping across x
 		{
-			pixels[j][i]->x = j*SCALE;
-			pixels[j][i]->y = i*SCALE;
-			pixels[j][i]->w = SCALE;
-			pixels[j][i]->h = SCALE;	
+			pixels[j][i].x = j*SCALE;
+			pixels[j][i].y = i*SCALE;
+			pixels[j][i].w = SCALE;
+			pixels[j][i].h = SCALE;	
 		} 
 	}
 
 	format = screen->format;
 	white = SDL_MapRGB(format, 255, 255, 255);
 	black = SDL_MapRGB(format, 0, 0, 0);
-
-	printf("Step 4 Success");	
-
-	for (int i = 0; i < 32; i++)		//looping across y
-	{
-		for (int j = 0; j < 64; j++)	//looping across x
-		{
-			gfx[j][i]= j%2;	
-		} 
-	}
 
 	SDL_Delay(1000);
 }
@@ -111,9 +102,10 @@ void drawGraphics()
 		for (int j = 0; j < 64; j++)	//looping across x
 		{
 			if (gfx[j][i] == 1)
-				SDL_FillRect(screen, pixels[j][i], white);
+				SDL_FillRect(screen, &pixels[j][i], white);
 			else
-				SDL_FillRect(screen, pixels[j][i], black);	
+				SDL_FillRect(screen, &pixels[j][i], black);
 		} 
 	}
+	SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
