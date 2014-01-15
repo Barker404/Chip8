90967bd033bcb50c84ca49597e1e9608c20f2470
Barker404 <lewisjbarker@btopenworld.com>
2014-01-15 00:54:02 +0000

    Added fps timing

--- main.c
+++ main.c
@@ -5,21 +5,27 @@
 
 #define SCALE 4;
 
-SDL_Surface* screen;
+SDL_Surface* screen; 		//Surface to draw on
 SDL_Event event;
-SDL_Rect pixels[64][32];
+SDL_Rect pixels[64][32];	//Actual rectangles to be drawn
 
-SDL_PixelFormat *format;
-
-unsigned int testoo;
+SDL_PixelFormat *format;	//Format of screen
 
 unsigned int white;
 unsigned int black; 
 
+unsigned int cycles; 		//Cycles passed
+unsigned long startTime;	//Time in ms since SDL initialized (taken at start of loop)
+unsigned int cycleTime;		//Time taken for one cycle
+
+unsigned long fpsStartTime;	//Time 60 frames ago
+unsigned int fps;			//Frames in last second
+
+
 int main(int argc, char **argv)
 {
 	//Setup rendering and input
-	testoo = 0;
+	cycles = 0;
 	setupGraphics();
 	setupInput();
 
@@ -30,6 +36,17 @@ int main(int argc, char **argv)
 	//Emulation loop
 	while(gameRunning)
 	{
+		//Get current time
+		startTime = SDL_GetTicks();
+
+		if (cycles % 60 == 0 && cycles != 0)
+		{
+			//Get fps over last 60 frames
+			fps = (60000 / (SDL_GetTicks() - fpsStartTime));
+			printf("%i\n", fps);
+			fpsStartTime = SDL_GetTicks();
+		}
+
 		//Emulate one cycle
 		emulateCycle();
 
@@ -39,7 +56,11 @@ int main(int argc, char **argv)
 
 		//Store key press state
 		setKeys();
-		testoo++;
+
+		cycles++;
+		//Wait until 16ms passed total (~60Hz)	
+		cycleTime = SDL_GetTicks() - startTime;
+		SDL_Delay(16-cycleTime);
 	}
 
 	SDL_FreeSurface(screen);
@@ -60,7 +81,7 @@ void setupGraphics()
 	//Set screen caption
 	SDL_WM_SetCaption("Chip8 Emulator", "Chip8 Emulator");
 		
-	//Set video mode to 64 x 32
+	//Set video mode to 64 x 32 times the scale
 	int h;
 	h = 32*SCALE;
 	int w;
@@ -72,6 +93,7 @@ void setupGraphics()
 		exit(1);
 	}
 
+	//Setup rectangles
 	for (int i = 0; i < 32; i++)		//looping across y
 	{
 		for (int j = 0; j < 64; j++)	//looping across x
@@ -83,11 +105,13 @@ void setupGraphics()
 		} 
 	}
 
+	//Setup colours
 	format = screen->format;
 	white = SDL_MapRGB(format, 255, 255, 255);
 	black = SDL_MapRGB(format, 0, 0, 0);
 
-	SDL_Delay(1000);
+	//Set inital (fake) fps
+	fps = 60;
 }
 
 void setupInput()
@@ -101,6 +125,13 @@ void drawGraphics()
 	{
 		for (int j = 0; j < 64; j++)	//looping across x
 		{
+			if(cycles % 60 == 0)
+			{
+				if (gfx[j][i] == 0)
+					gfx[j][i] = 1;
+				else
+					gfx[j][i] = 0;
+			}
 			if (gfx[j][i] == 1)
 				SDL_FillRect(screen, &pixels[j][i], white);
 			else
